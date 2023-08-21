521358bfeed7b36e9ff8f6590427d4c6d592844b
Maria <zhmasha2505@gmail.com>
2023-08-21 15:10:43 +0300

    Modify homework_15 after completing #16

--- homework_15/airbnb.sql
+++ homework_15/airbnb.sql
@@ -1,30 +1,31 @@
-CREATE TABLE Users (
-  id serial PRIMARY KEY,
-  user_type CHAR,
-  created_at timestamp
-);
-
+-- Creating tables
 CREATE TABLE Hosts (
-  id serial,
-  FOREIGN KEY (id) REFERENCES Users (id),
-  created_at timestamp
-);
-
-CREATE TABLE Guests (
-  id serial,
-  room_id serial,
-  FOREIGN KEY (id) REFERENCES Users (id), (room_id) REFERENCES Rooms (id),
-  created_at timestamp
+  id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  name CHAR(60),
+  budget float,
+  created_at timestamp without time zone default (now() at time zone 'utc')
 );
 
 CREATE TABLE Rooms (
-  id serial PRIMARY KEY,
-  host_id serial,
+  id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  host_id uuid,
   FOREIGN KEY (host_id) REFERENCES Hosts (id),
   available Boolean,
+  cost_per_night float,
   amount_residents integer,
-  price float,
   AC Boolean,
   Refrigerator Boolean,
-  created_at timestamp
+  created_at timestamp without time zone default (now() at time zone 'utc')
+); 
+
+CREATE TABLE Guests (
+  id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  first_name CHAR(50),
+  last_name CHAR(50), 
+  room_id uuid,
+  FOREIGN KEY (room_id) REFERENCES Rooms (id),
+  num_of_night int,
+  total_fee float,
+  num_of_reservations int,
+  created_at timestamp without time zone default (now() at time zone 'utc')
 );
