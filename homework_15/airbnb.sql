943a7a15b7831f45b0761168db5d33a048c7d8d6
Maria <zhmasha2505@gmail.com>
2023-09-03 17:26:48 +0300

    Modify after review

--- homework_15/airbnb.sql
+++ homework_15/airbnb.sql
@@ -1,31 +1,52 @@
--- Creating tables
 CREATE TABLE Hosts (
-  id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   name CHAR(60),
-  budget float,
-  created_at timestamp without time zone default (now() at time zone 'utc')
+  budget FLOAT,
+  average_rating FLOAT,
+  created_at TIMESTAMP without time zone default (now() at time zone 'utc')
 );
 
 CREATE TABLE Rooms (
-  id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   host_id uuid,
-  FOREIGN KEY (host_id) REFERENCES Hosts (id),
-  available Boolean,
-  cost_per_night float,
-  amount_residents integer,
-  AC Boolean,
-  Refrigerator Boolean,
-  created_at timestamp without time zone default (now() at time zone 'utc')
-); 
+  FOREIGN KEY (host_id) REFERENCES Hosts(id)
+  available BOOLEAN,
+  cost_per_night FLOAT,
+  amount_residents INTEGER,
+  AC BOOLEAN,
+  Refrigerator BOOLEAN,
+  created_at TIMESTAMP without time zone default (now() at time zone 'utc')
+);
 
 CREATE TABLE Guests (
-  id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   first_name CHAR(50),
-  last_name CHAR(50), 
-  room_id uuid,
-  FOREIGN KEY (room_id) REFERENCES Rooms (id),
-  num_of_night int,
-  total_fee float,
-  num_of_reservations int,
-  created_at timestamp without time zone default (now() at time zone 'utc')
+  last_name CHAR(50),
+  num_of_reservations INT,
+  created_at TIMESTAMP without time zone default (now() at time zone 'utc')
+);
+
+CREATE TABLE Reservation (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  guest_id uuid,
+  room_id uuid,  
+  FOREIGN KEY (guest_id) REFERENCES Guests(id),
+  FOREIGN KEY (room_id) REFERENCES Rooms(id),
+  payment_status BOOLEAN,
+  total_fee FLOAT,
+  check_in_date TIMESTAMP,
+  check_out_date TIMESTAMP,
+  num_of_night INT,
+  created_at TIMESTAMP without time zone default (now() at time zone 'utc')
+);
+
+CREATE TABLE Review (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  guest_id uuid,
+  host_id uuid,
+  FOREIGN KEY (guest_id) REFERENCES Guests(id),
+  FOREIGN KEY (host_id) REFERENCES Hosts(id),
+  ratings INT,
+  comment CHAR(200),
+  created_at TIMESTAMP without time zone default (now() at time zone 'utc')
 );
